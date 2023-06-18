0a441776368f7a1e19555ffbf4b7da497dde142e
Jakef14 <135998840+JakeFranklin14@users.noreply.github.com>
2023-06-18 16:44:01 -0400

    new commit

---
+++ CameraAppV2/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import TabSelector from './components/TabSelector';
+
+export default function App() {
+  // insert camera permissions here
+  return (
+    <TabSelector/>
+  );
+}
--- CameraAppV2/components/ExpandableValues.js
+++ CameraAppV2/components/ExpandableValues.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Pressable, Text, Alert, StyleSheet, View } from 'react-native';
-import { getWeather, dailyForecast, showWeather, getLocation } from 'react-native-weather-api';
 import Dialog from "react-native-dialog";
 
 const ExpandableValues = ({name}) => {
@@ -13,31 +12,6 @@ const ExpandableValues = ({name}) => {
     );
 };
 
-const Weather = () => {
-    const [visible, setVisible] = useState(false);
-    let temp;
-    let wind;
-
-    getWeather({
-			
-        key: "e0b4b0b0c0mshb0b0b0b0b0b0b0b0b0b0b0b0b0b0b0",
-        city: "Newton",
-        country: "USA"
-
-    }).then(() => {
-        let data = new showWeather();
-        temp = data.temp;
-        wind = data.wind;
-        <Dialog.Container visible={visible}>
-            <Dialog.Title>Weather</Dialog.Title>
-            <Dialog.Description>
-                <Text>Temperature: {temp}</Text>
-                <Text>Wind: {wind}</Text>
-            </Dialog.Description>
-            <Dialog.Button label="OK" onPress={() => setVisible(false)} />
-        </Dialog.Container>
-    });
-};
 
 
 export default ExpandableValues;
